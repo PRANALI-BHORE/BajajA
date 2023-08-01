002d15a7ebab79f3467dc29ca2fdde572e5443c0
PRANALI-BHORE <pranalibhore07@gmail.com>
2023-08-01 17:58:55 +0530

    pushing code to git

---
+++ TestCases_JUnit-1/src/main/java/com/prowings/program/addition.java
@@ -0,0 +1,16 @@
+package com.prowings.program;
+
+public class addition {
+	
+	public static void main(String[] args) {
+		addition();
+	}
+	
+	public static int addition() {
+		int a=10,b=20;
+		int result=a+b;
+		System.out.println("addition is: "+result);
+		return result;
+	}
+
+}
