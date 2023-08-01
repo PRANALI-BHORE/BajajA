47471b197b7ed827052f42504d56864da3df5d6c
PRANALI-BHORE <pranalibhore07@gmail.com>
2023-08-01 18:09:47 +0530

    adding substraction method and did changes in addition method

--- TestCases_JUnit-1/src/main/java/com/prowings/program/addition.java
+++ TestCases_JUnit-1/src/main/java/com/prowings/program/addition.java
@@ -2,14 +2,24 @@ package com.prowings.program;
 
 public class addition {
 	
+	static int a=10,b=20,result;
+	
 	public static void main(String[] args) {
 		addition();
+		substraction();
 	}
 	
 	public static int addition() {
-		int a=10,b=20;
-		int result=a+b;
+		
+		result=a+b;
 		System.out.println("addition is: "+result);
+		return result;
+	}
+	
+	public static int substraction() {
+		
+		result=b-a;
+		System.out.println("substraction is: "+result);
 		return result;
 	}
 
